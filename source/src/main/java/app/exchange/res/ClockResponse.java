ca1e9bf141225f127a8049532c02752c20139980
aemiranda7 <aemiranda7@gmail.com>
2021-06-01 18:54:14 +0100

    clock response
    Co-authored-by: João Azevedo <devzizu@users.noreply.github.com>

--- source/src/main/java/app/exchange/res/ClockResponse.java
+++ source/src/main/java/app/exchange/res/ClockResponse.java
@@ -3,5 +3,11 @@ package app.exchange.res;
 public class ClockResponse extends MessageWrapper {
 
     public long clockValue;
+    public String nodeID;
+
+    public ClockResponse(long clockValue, String nodeID) {
+        this.clockValue = clockValue;
+        this.nodeID = nodeID;
+    }
 
 }
--- source/src/main/java/app/node/runnable/RepRunnable.java
+++ source/src/main/java/app/node/runnable/RepRunnable.java
@@ -1,23 +1,29 @@
 package app.node.runnable;
 
+import java.io.Serializable;
+
 import org.zeromq.SocketType;
 import org.zeromq.ZContext;
 import org.zeromq.ZMQ;
 
 import app.central.usernode.NodeNetwork;
 import app.exchange.*;
+import app.exchange.req.*;
+import app.node.persist.NodeDatabase;
+import app.util.data.Serialization;
 
 public class RepRunnable implements Runnable {
     
     private ZContext context;
     private NodeNetwork nodeNetwork;
+    private NodeDatabase nodeDatabase;
 
 
 
-
-    public RepRunnable(ZContext context, NodeNetwork nodeNetwork) {
+    public RepRunnable(ZContext context, NodeNetwork nodeNetwork, NodeDatabase nodeDatabase) {
         this.context = context;
         this.nodeNetwork = nodeNetwork;
+        this.nodeDatabase = nodeDatabase;
     }
 
 
@@ -34,6 +40,22 @@ public class RepRunnable implements Runnable {
                 byte[] msg = repSocket.recv();
 
                 MessageWrapper message = (MessageWrapper) Serialization.deserialize(msg);
+
+                if (message instanceof ClockRequest){
+                    ClockRequest rMessage = (ClockRequest) message;
+
+                    message = new ClockResponse(nodeDatabase.subscriptionClocks.get(rMessage.nodeID),rMessage.nodeID);
+                }
+
+                if (message instanceof RecoverRequest){
+                    RecoverRequest rMessage = (RecoverRequest) message;
+
+                    //Todo Logica do recover
+                }
+
+                msg = Serialization.serialize(message);
+
+                repSocket.send(msg);
 
             }
 
