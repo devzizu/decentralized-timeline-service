fb68bdfaa6a659dcb97e4dba8c02395e6ce9c8fd
devzizu <jazevedo960@gmail.com>
2021-06-03 09:26:51 +0100

    added a script to run  a generic node

--- source/run.sh
+++ source/run.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
-BASE_PORT=10000
-
 case $1 in
     "central")
         make run main="app.central.Central" args="-central central1"
         ;;
     
     "node")
-        PUB=BASE_PORT+
-        PULL=
-        REPLY=
-        make run main="app.node.Node" args="--register -node node1 -pub 10000 -pull 10001 -reply 10002"
+        BASE=100
+        BASE+=$(($2-1))
+        BASE+="0"
+        PUB=$(($BASE+1))
+        PULL=$(($BASE+2))
+        REPLY=$(($BASE+3))
+        make run main="app.node.Node" args="--$3 -node node1 -pub $PUB -pull $PULL -reply $REPLY"
         ;;
 esac
